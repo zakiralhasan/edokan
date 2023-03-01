66487405b693ffc422d39a21e18869903ad27fd5
zakiralhasan <zakiralhasan.bd@gmail.com>
2023-03-01 14:30:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import '../src/styles/sass/global.css'
+import './App.css';
+import Home from './pages/home-page/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+
+    </div>
+  );
+}
+
+export default App;
